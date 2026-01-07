ebca17110afb46f5f27941b5b43e9f73814aa581
ransomjam99 <ransomjam99@gmail.com>
2026-01-07 08:36:54 +0100

    Add new Vercel URL to CORS origins

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
   'https://ubatechcamp-jt7oqhsf0-jam-ransoms-projects.vercel.app',
   'https://ubatechcamp-4rlcmcgek-jam-ransoms-projects.vercel.app',
   'https://ubatechcamp-cfngww8j5-jam-ransoms-projects.vercel.app',
+  'https://ubatechcamp-o74mged08-jam-ransoms-projects.vercel.app',
 ];
 
 app.use(cors({
